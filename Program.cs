eddb08e035a0dbe29bc2c6b64c26e3961354670c
Michael Burch <michaelburch@outlook.com>
2020-11-02 15:30:10 -0600

    start port to statiq

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace michaelburchdotnet
+{
+  public class Program
+  {
+    public static async Task<int> Main(string[] args) =>
+      await Bootstrapper
+        .Factory
+        .CreateWeb(args)
+        .AddShortcode<CaptionImage>()
+        .AddShortcode<CaptionImageRight>()
+        .AddShortcode<CaptionImageLeft>()
+        .RunAsync();
+  }
+}
