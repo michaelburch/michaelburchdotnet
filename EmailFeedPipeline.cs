75fb5051fb695d0842a23bf52410110a9582f83a
Michael Burch <me@michaelburch.net>
2021-04-13 21:44:22 -0500

    feed pipeline updates (#70)
    * parity with wyam site
    
    * update version
    
    * update pipeline with new branch name
    
    * draft python functions
    
    * add serverless python post
    
    * fix mailto link
    
    * update about
    
    * use post excerpt for feeditemdesc
    
    * latest statiq, fix mailto link in markdown
    
    * fix feeditem desc

--- EmailFeedPipeline.cs
+++ EmailFeedPipeline.cs
@@ -75,10 +75,10 @@ namespace Statiq.Web.Pipelines
                         {
                             generateFeeds = generateFeeds.WithItemTitle(Config.FromDocument(doc => doc.GetString(WebKeys.FeedItemTitle)));
                         }
-                        if (feedDoc.ContainsKey(WebKeys.FeedItemDescription))
-                        {
-                            generateFeeds = generateFeeds.WithItemDescription(Config.FromDocument(doc => doc.GetString(WebKeys.FeedItemDescription)));
-                        }
+
+                            generateFeeds = generateFeeds.WithItemDescription(Config.FromDocument(doc => doc.GetString("Excerpt",WebKeys.FeedItemDescription)));
+                        
+
                         if (feedDoc.ContainsKey(WebKeys.FeedItemAuthor))
                         {
                             generateFeeds = generateFeeds.WithItemAuthor(Config.FromDocument(doc => doc.GetString(WebKeys.FeedItemAuthor)));
